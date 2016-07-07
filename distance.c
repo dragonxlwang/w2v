bd0b6dbee55a927e37ccaadc99f3aabfa50eac8d
Xiaolong Wang <dragonxlwang@gmail.com>
2016-07-06 21:32:10 -0500

    commit

--- distance.c
+++ distance.c
@@ -12,14 +12,15 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+#include <ctype.h>
+#include <malloc.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
-#include <malloc.h>
 
-const long long max_size = 2000;         // max length of strings
-const long long N = 40;                  // number of closest words that will be shown
-const long long max_w = 50;              // max length of vocabulary entries
+const long long max_size = 2000;  // max length of strings
+const long long N = 40;           // number of closest words that will be shown
+const long long max_w = 50;       // max length of vocabulary entries
 
 int main(int argc, char **argv) {
   FILE *f;
@@ -28,11 +29,12 @@ int main(int argc, char **argv) {
   char file_name[max_size], st[100][max_size];
   float dist, len, bestd[N], vec[max_size];
   long long words, size, a, b, c, d, cn, bi[100];
-  char ch;
   float *M;
   char *vocab;
   if (argc < 2) {
-    printf("Usage: ./distance <FILE>\nwhere FILE contains word projections in the BINARY FORMAT\n");
+    printf(
+        "Usage: ./distance <FILE>\nwhere FILE contains word projections in the "
+        "BINARY FORMAT\n");
     return 0;
   }
   strcpy(file_name, argv[1]);
@@ -47,7 +49,8 @@ int main(int argc, char **argv) {
   for (a = 0; a < N; a++) bestw[a] = (char *)malloc(max_size * sizeof(char));
   M = (float *)malloc((long long)words * (long long)size * sizeof(float));
   if (M == NULL) {
-    printf("Cannot allocate memory: %lld MB    %lld  %lld\n", (long long)words * size * sizeof(float) / 1048576, words, size);
+    printf("Cannot allocate memory: %lld MB    %lld  %lld\n",
+           (long long)words * size * sizeof(float) / 1048576, words, size);
     return -1;
   }
   for (b = 0; b < words; b++) {
@@ -71,7 +74,7 @@ int main(int argc, char **argv) {
     printf("Enter word or sentence (EXIT to break): ");
     a = 0;
     while (1) {
-      st1[a] = fgetc(stdin);
+      st1[a] = toupper(fgetc(stdin));
       if ((st1[a] == '\n') || (a >= max_size - 1)) {
         st1[a] = 0;
         break;
@@ -96,7 +99,8 @@ int main(int argc, char **argv) {
     }
     cn++;
     for (a = 0; a < cn; a++) {
-      for (b = 0; b < words; b++) if (!strcmp(&vocab[b * max_w], st[a])) break;
+      for (b = 0; b < words; b++)
+        if (!strcmp(&vocab[b * max_w], st[a])) break;
       if (b == words) b = -1;
       bi[a] = b;
       printf("\nWord: %s  Position in vocabulary: %lld\n", st[a], bi[a]);
@@ -106,7 +110,10 @@ int main(int argc, char **argv) {
       }
     }
     if (b == -1) continue;
-    printf("\n                                              Word       Cosine distance\n------------------------------------------------------------------------\n");
+    printf(
+        "\n                                              Word       Cosine "
+        "distance\n------------------------------------------------------------"
+        "------------\n");
     for (a = 0; a < size; a++) vec[a] = 0;
     for (b = 0; b < cn; b++) {
       if (bi[b] == -1) continue;
@@ -120,7 +127,8 @@ int main(int argc, char **argv) {
     for (a = 0; a < N; a++) bestw[a][0] = 0;
     for (c = 0; c < words; c++) {
       a = 0;
-      for (b = 0; b < cn; b++) if (bi[b] == c) a = 1;
+      for (b = 0; b < cn; b++)
+        if (bi[b] == c) a = 1;
       if (a == 1) continue;
       dist = 0;
       for (a = 0; a < size; a++) dist += vec[a] * M[a + c * size];
@@ -136,7 +144,10 @@ int main(int argc, char **argv) {
         }
       }
     }
-    for (a = 0; a < N; a++) printf("%50s\t\t%f\n", bestw[a], bestd[a]);
+    for (a = 0; a < N; a++) {
+      for (c = 0; c < strlen(bestw[a]); c++) bestw[a][c] = tolower(bestw[a][c]);
+      printf("%50s\t\t%f\n", bestw[a], bestd[a]);
+    }
   }
   return 0;
 }
